74540a4dd863ac25c4af019201ebee6e921ab1eb
cemetry <matvey.marusik@mail.ru>
2022-04-15 22:30:35 +0300

    List creation

--- app/src/main/java/com/example/metalllcore/MainActivity.java
+++ app/src/main/java/com/example/metalllcore/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.metalllcore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] artistTrack = { "Wildways — Breathless", "Wildways — Lost", "Architects — Giving Blood", "Architects — Animals", "Architects — Black Lungs", "ТАйМСКВЕР — Время первых", "Bad Omens — Like A Villain", "Bad Omens — THE DEATH OF PEACE OF MIND", "Bring Me The Horizon — The House of Wolves", "Bring Me The Horizon — Shadow Moses"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView artistTrackList = findViewById(R.id.artistTrackList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artistTrack);
+
+        artistTrackList.setAdapter(adapter);
     }
 }
