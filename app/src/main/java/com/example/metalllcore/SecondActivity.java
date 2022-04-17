7b54db9cb91f7d8850974c1ff0ec004f394b49b3
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-04-17 19:24:07 +0300

    version 1.0 (without translate)

--- app/src/main/java/com/example/metalllcore/SecondActivity.java
+++ app/src/main/java/com/example/metalllcore/SecondActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
-    Map<String, Integer> dictionary = new HashMap<>();
+    Map<String, Integer> dictionaryOfTracks = new HashMap<>();
+    Map<String, Integer> dictionaryOfCovers = new HashMap<>();
     TextView textView;
     TextView textView2;
 
@@ -22,20 +24,47 @@ public class SecondActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
         textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setHorizontallyScrolling(true);
+        ImageView imageView = findViewById(R.id.imageView);
 
-        dictionary.put("Wildways — Breathless", R.string.Wildways_Breathless);
-        dictionary.put("Wildways — Lost", R.string.Wildways_Lost);
-        dictionary.put("Architects — Animals", R.string.Architects_Animals);
-        dictionary.put("Architects — Giving Blood", R.string.Architects_Giving_Blood);
-        dictionary.put("Architects — Black Lungs", R.string.Architects_Giving_Blood);
-        dictionary.put("ТАйМСКВЕР — Время первых", R.string.ТАйМСКВЕР_Время_первых);
-        dictionary.put("Bad Omens — Like A Villain", R.string.Bad_Omens_Like_A_Villain);
-        dictionary.put("Bad Omens — THE DEATH OF PEACE OF MIND", R.string.Bad_Omens_THE_DEATH_OF_PEACE_OF_MIND);
-        dictionary.put("Bring Me The Horizon — The House of Wolves", R.string.Bring_Me_The_Horizon_Th_House_of_Wolves);
-        dictionary.put("Bring Me The Horizon — Shadow Moses", R.string.Bring_Me_The_Horizon_Shadow_Moses);
+        dictionaryOfTracks.put("Wildways — Breathless", R.string.Wildways_Breathless);
+        dictionaryOfTracks.put("Wildways — Lost", R.string.Wildways_Lost);
+        dictionaryOfTracks.put("Architects — Animals", R.string.Architects_Animals);
+        dictionaryOfTracks.put("Architects — Giving Blood", R.string.Architects_Giving_Blood);
+        dictionaryOfTracks.put("Architects — Black Lungs", R.string.Architects_Black_Lungs);
+        dictionaryOfTracks.put("ТАйМСКВЕР — Время первых", R.string.ТАйМСКВЕР_Время_первых);
+        dictionaryOfTracks.put("Bad Omens — Like A Villain", R.string.Bad_Omens_Like_A_Villain);
+        dictionaryOfTracks.put("Bad Omens — THE DEATH OF PEACE OF MIND", R.string.Bad_Omens_THE_DEATH_OF_PEACE_OF_MIND);
+        dictionaryOfTracks.put("Bring Me The Horizon — The House of Wolves", R.string.Bring_Me_The_Horizon_Th_House_of_Wolves);
+        dictionaryOfTracks.put("Bring Me The Horizon — Shadow Moses", R.string.Bring_Me_The_Horizon_Shadow_Moses);
 
-        textView2.setText(arguments.getString("track"));
-        textView.setText(dictionary.get(arguments.getString("track")));
+        dictionaryOfCovers.put("Wildways — Breathless", R.drawable.day_x);
+        dictionaryOfCovers.put("Wildways — Lost", R.drawable.day_x);
+        dictionaryOfCovers.put("Architects — Animals", R.drawable.for_those_that_wish_to_exist);
+        dictionaryOfCovers.put("Architects — Giving Blood", R.drawable.for_those_that_wish_to_exist);
+        dictionaryOfCovers.put("Architects — Black Lungs", R.drawable.for_those_that_wish_to_exist);
+        dictionaryOfCovers.put("ТАйМСКВЕР — Время первых", R.drawable.time_of_the_first);
+        dictionaryOfCovers.put("Bad Omens — Like A Villain", R.drawable.the_death_of_peace_of_mind);
+        dictionaryOfCovers.put("Bad Omens — THE DEATH OF PEACE OF MIND", R.drawable.the_death_of_peace_of_mind);
+        dictionaryOfCovers.put("Bring Me The Horizon — The House of Wolves", R.drawable.sepmeternal);
+        dictionaryOfCovers.put("Bring Me The Horizon — Shadow Moses", R.drawable.sepmeternal);
+
+        imageView.setImageResource(dictionaryOfCovers.get(arguments.getString("track")));
+
+        String trackArray[] = (arguments.getString("track")).split(" ");
+        String track = "";
+        for (String part: trackArray) {
+            if(part.equals("—")){
+                track +="\n";
+            }
+            else {
+                track += part + " ";
+            }
+
+        }
+
+        textView2.setText(track);
+        textView.setText(dictionaryOfTracks.get(arguments.getString("track")));
 
     }
 }
